b7402868c1e5d6147f6610d641ea9f393308e0fa
Kaushal Chaudhary <kaushalchau.2007@gmail.com>
2026-06-25 13:21:55 +0530

    Add files via upload

---
+++ t4/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from account.views import dashboard,register,user_login,user_logout
+
+urlpatterns = [
+    path("dashboard/",dashboard,name='dashboard'),
+    path("register/",register,name='register'),
+    path("login/",user_login,name='login'),
+    path("logout/",user_logout,name='logout'),
+]
