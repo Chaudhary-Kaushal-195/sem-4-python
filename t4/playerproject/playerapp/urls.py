fd87d7803cd8a92b22aed7ecd6c50a483c58a52b
Kaushal Chaudhary <kaushalchau.2007@gmail.com>
2026-06-29 10:38:04 +0530

    Add files via upload

---
+++ t4/playerproject/playerapp/urls.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+# from django.urls import path,include
+# from playerapp.views import home, welcome
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('home/',home,name='home')
+
+# ]
---
+++ t4/playerproject/playerapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from playerapp.models import Player
+
+# Create your views here.
+def home(request):
+    search=request.GET.get('name')
+    if search:
+        play=Player.objects.filter(name__icontains=search)
+    else:
+        play=Player.objects.all()
+    return render(request,'home.html',{'play':play})
+def welcome(request):
+    return render(request,'welcome.html')
+def add(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        innings=request.POST['innings']
+        runs=request.POST['runs']
+        Player.objects.create(name=name,innings=innings,runs=runs)
+        return redirect('home')
+    return render(request,'add.html')
+def edit(request,id):
+    play=get_object_or_404(Player,id=id)
+    if request.method=="POST":
+        play.name=request.POST['name']
+        play.innings=request.POST['innings']
+        play.runs=request.POST['runs']
+        play.save()
+        return redirect('home')
+    return render(request,'edit.html',{'play':play})
+
+def dele(request,id):
+    play=get_object_or_404(Player,id=id)
+    play.delete()
+    return redirect('home')
